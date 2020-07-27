fbfc23502ac00d1b165fcaa37a4b8479bf0b096a
zhmhbest <zhmhbest@gmail.com>
2020-07-27 18:00:09 +0800

    词汇

---
+++ notes/data/vocabulary/merge.py
@@ -0,0 +1,23 @@
+"""
+    将多个单词CSV文件合成一个CSV文件
+"""
+import numpy as np
+import pandas as pd
+import os
+
+if __name__ == '__main__':
+    buffer = np.array([[None, None]])
+    for filename in os.listdir('./'):
+        split_filename = os.path.splitext(filename)
+        if '.csv' == split_filename[-1].lower() and 'index' != split_filename[0].lower():
+            tmp = pd.read_csv(filename, skiprows=1, names=['v', 't']).to_numpy()
+            buffer = np.vstack((buffer, tmp))
+    df = pd.DataFrame(buffer, columns=['v', 't']).drop(0)
+
+    # 按字母表排序
+    df.sort_values('v', inplace=True)
+
+    # 去除重复单词（保留第一次出现）
+    df.drop_duplicates(subset='v', inplace=True)
+
+    df.to_csv('./index.csv', index=False, header=['单词', '解释'])
---
+++ notes/vocabulary.md
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://zhmhbest.gitee.io/hellomathematics/style/index.css">
+
+# [每日词汇](./index.html)
+
+@import "./data/vocabulary/index.csv"

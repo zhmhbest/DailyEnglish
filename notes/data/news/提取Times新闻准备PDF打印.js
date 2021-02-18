958f1e53d67f8bd90e3db536cdbec37bdcc892df
zhmhbest <zhmhbest@gmail.com>
2021-02-19 05:01:54 +0800

    update

--- notes/data/news/提取Times新闻准备PDF打印.js
+++ notes/data/news/提取Times新闻准备PDF打印.js
@@ -1,19 +1,47 @@
-// 提取主内容
-document.querySelector('main').innerHTML =
-    document.querySelector('.headline-and-lead-media').innerHTML +
-    document.querySelector('.article-content').innerHTML
-document.querySelector('body').innerHTML = document.querySelector('main').parentElement.innerHTML
-
-// 移除底部栏
-document.querySelector('.article-bottom').remove()
-document.querySelector('.most-popular-feed').remove()
-document.querySelector('.newsletter-callout').remove()
-document.querySelector('.bottom-recirc').remove()
-
-// 移除广告
-for(item of document.querySelectorAll('.ad-container')) item.remove()
-
-// 优化
-for(item of document.querySelectorAll('p a')) item.parentElement.innerHTML = item.parentElement.innerText
-for(item of document.querySelectorAll('iframe')) item.remove()
-for(item of document.querySelectorAll('script')) item.remove()
+const mainElement = document.querySelector("main");
+const mainRoot = mainElement.parentElement.parentElement;
+const countMax=10;
+let countLoop=1; const intervalMain = setInterval(() => {
+    console.log(`${countLoop}/${countMax}`);
+    // 提取Root
+    for(item of document.body.children) {
+        if(mainRoot !== item && null !== item) item.remove();
+    }
+    // 提取Main
+    for(item of mainElement.children) {
+        if(item.className !== 'headline-and-lead-media' && item.className !== 'article-column-container' ) item.remove();
+    }
+    // 优化Head
+    for(item of document.head.children) {
+        if(item === null) continue;
+        if('TITLE'===item.tagName.toUpperCase()) continue;
+        if('LINK'===item.tagName.toUpperCase() && 'stylesheet'===item.rel.toLowerCase()) continue;
+        item.remove();
+    }
+    if(countLoop>=countMax) {
+        clearInterval(intervalMain);
+        // 移除lead-media
+        document.querySelector("div.lead-media").remove();
+        // 移除段内广告
+        for(item of document.querySelectorAll('.ad-container')) item.remove();
+        // 移除侧边栏
+        document.querySelector("div.sidebar-right").remove();
+        // 移除Subscribe
+        document.querySelector("div.subscription-cta").remove();
+        // 移除Sign
+        document.querySelector("div.newsletter-signup-inline").parentElement.remove();
+        // 移除feedback
+        document.querySelector("p.author-feedback-text").remove();
+        // 移除Share
+        document.querySelector('.article-bottom').remove();
+        // 移除Read More
+        document.querySelector("div.bottom-recirc.padded-mobile").remove();
+        // 禁止连接跳转
+        for(item of document.querySelectorAll('p a')) { item.href='javascript:'; item.target='_self' }
+        // 优化
+        for(item of document.querySelectorAll('html iframe')) item.remove();
+        for(item of document.querySelectorAll('html script')) item.remove();
+        for(item of document.querySelectorAll('html style')) item.remove();
+        console.log("Done!");
+    } else countLoop++;
+}, 500);
